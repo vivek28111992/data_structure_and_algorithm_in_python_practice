cd39eb0d28c84bc3bb3672e421b0d8a17c9b4ff2
Vivek <pawarvivek29@gmail.com>
2018-08-15 14:40:09 +0530

    default parameter in function

---
+++ 1. Python Primer/informationPassing.py
@@ -0,0 +1,13 @@
+# Default Parameter Values
+# A function that computes a student's GPA with a point value system that can be customized as an optimal parameter.
+
+def compute_gpa(grades, points={'A+': 4.0, 'A': 4.0, 'A-': 3.67, 'B+': 3.33, 'B': 3.0, 'B-': 2.67, 'C+': 2.33, 'C': 2.0, 'C-': 1.67, 'D+': 1.33, 'D': 1.0, 'F': 0.0 }):
+    num_courses = 0
+    total_points = 0
+    for g in grades:
+        if g in points:
+            num_courses += 1
+            total_points += points[g]
+    return total_points / num_courses
+
+print('{0:.3}'.format(compute_gpa(['B', 'A', 'A+', 'B-'])))
