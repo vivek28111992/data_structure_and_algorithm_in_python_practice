4494c5748927daf2311f4af12a04baf7163c3431
Vivek <pawarvivek29@gmail.com>
2018-08-30 03:07:38 +0530

    array based programs

---
+++ 4. Array-Based Sequences/insertion_sort.py
@@ -0,0 +1,16 @@
+# Insertion Sort
+# Python code for performing insertion-sort on a list.
+
+def insertion_sort(A):
+    """Sort list of comparable elements into nondecreasing order."""
+    for k in range(1, len(A)):                  # from 1 to n-1
+        cur = A[k]                              # current element to be inserted
+        j = k                                   # find correct index j for current
+        while j > 0 and A[j - 1] > cur:         # element A[j-1] must be after current
+            A[j] = A[j - 1]
+            j -= 1
+        A[j] = cur                              # cur is now in the right place
+    return A
+
+res = insertion_sort([1, 3, 6, 8, 2, 5, 0])
+print(res)
